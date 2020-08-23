8b82a76616d91bbcbfa65aaca53026646c266352
junji9072 <flasusmaga@gmail.com>
2020-08-23 15:44:09 +0900

    배포오오오 자소서어어얼

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jasoseolproject.wsgi --log-file -
--- jasoseolproject/settings.py
+++ jasoseolproject/settings.py
@@ -1,5 +1,5 @@
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DjANGO_DEBUG',False))
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +136,6 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
